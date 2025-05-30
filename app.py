c50bdf6f5ee587376cc155bca5fd42dac60b8437
ogabrieldias <gabrielbielbernardino@gmail.com>
2025-05-30 00:30:38 +0000

    Corrige porta dinâmica para o Replit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, jsonify, url_for
 import requests
+import os
 
 app = Flask(__name__)
 
@@ -24,5 +25,9 @@ def get_weather():
     return jsonify(weather_data)
 
 
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=3000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
